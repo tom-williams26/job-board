2081b89eaa907c52cec722e46299ab94d249fdb0
Tom Williams <tom.williams0593@outlook.com>
2025-05-04 04:35:58 +0100

    Created Job model and migration.

---
+++ app/Models/Job.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    /** @use HasFactory<\Database\Factories\JobFactory> */
+    use HasFactory;
+
+    public static array $experience = ['entry', 'intermediate', 'senior'];
+    public static array $category = [
+        'IT',
+        'Finance',
+        'Sales',
+        'Marketing'
+    ];
+}
