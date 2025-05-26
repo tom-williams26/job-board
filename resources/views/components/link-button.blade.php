883bf853924763cddc65e2fc49e461cc17be1779
Tom Williams <tom.williams0593@outlook.com>
2025-05-26 04:07:12 +0100

    Create a job-card and link-button component. The user show be able to click on the "show" link button and it will given them further detail about a specific job.

---
+++ resources/views/components/link-button.blade.php
@@ -0,0 +1,4 @@
+<a class="rounded-md border border-slate-300 bg-white px-2.5 py-1.5 text-center text-sm font-semibold text-black shadow-sm hover:bg-slate-500"
+    href="{{ $href }}">
+    {{ $slot }}
+</a>
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('', fn() => to_route('jobs.index'));
 
 Route::resource('jobs', JobController::class)
-    ->only(['index']);
+    ->only(['index', 'show']);
