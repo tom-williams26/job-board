ab950cc7e05f7fdf475a6638b80b41c7822b6deb
Tom Williams <tom.williams0593@outlook.com>
2025-05-26 03:44:33 +0100

    Pulled data from the database to display on the UI (jobs) via blade components. Added a tag component to prevent repeated code and making use of dynamic values.

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,3 @@
-<div {{ $attributes->class(['rounded-mb border border-slate-300 bg-white p-4 shadow-sm']) }}>
+<article {{ $attributes->class(['rounded-mb border border-slate-300 bg-white p-4 shadow-sm']) }}>
     {{ $slot }}
-</div>
-</div>
+</article>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('', fn() => to_route('jobs.index'));
+
 Route::resource('jobs', JobController::class)
     ->only(['index']);
