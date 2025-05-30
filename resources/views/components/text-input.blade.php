f3784aaa75eb88385fa477af98c2030523f3a529
Tom Williams <tom.williams0593@outlook.com>
2025-05-30 18:38:53 +0100

    Added tailwind config file in order to use a form plugin from tailwindcss docs. Created a text-input component to be used for filtering jobs and selecting a salary range.

---
+++ resources/views/components/text-input.blade.php
@@ -0,0 +1,8 @@
+<input
+    type="text"
+    placeholder="{{ $placeholder }}"
+    name="{{ $name }}"
+    value="{{ $value }}"
+    id="{{ $name }}"
+    class="placeholder:text-slatee-400 w-full rounded-md border-0 px-2.5 py-1.5 text-sm ring-1 ring-slate-300 focus:ring-2"
+/>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const forms = require('@tailwindcss/forms');
+
+module.exports = {
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+    plugins: [
+      require('@tailwindcss/forms')
+  ],
+};
