609b90c1575d572a8b656e56ec36230cc09e1259
Tom Williams <tom.williams0593@outlook.com>
2025-06-19 19:51:48 +0100

    Logs out users that are authenticated. Invalidating the session and regenerates the CSRF token preventing any session fixation attacks.  Within the blade file a DELETE reuest is made to auth.destroy upon logging out. Added logout route and a additional route for executing the destroy method in the AuthController.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,7 +15,34 @@
     <body
         class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-100 from-10% via-sky-100 via-30% to-emerald-100 to-90% text-slate-700"
     >
-        {{ auth()->user()->name ?? 'Guest' }}
+        <nav class="mb-8 flex justify-between text-lg font-medium">
+            <ul class="flex space-x-2">
+                <a href="{{ route('jobs.index') }}">Home</a>
+            </ul>
+
+            <ul class="flex space-x-2">
+                @auth
+                    <li>
+                        {{ auth()->user()->name ?? 'Anonymous' }}
+                    </li>
+                    <li>
+                        <form
+                            action="{{ route('auth.destroy') }}"
+                            method="POST"
+                        >
+                            @csrf
+                            @method('DELETE')
+                            <button>Logout</button>
+                        </form>
+                    </li>
+                @else
+                    <li>
+                        <a href="{{ route('auth.create') }}">Sign in</a>
+                    </li>
+                @endauth
+            </ul>
+
+        </nav>
         {{ $slot }}
     </body>
 
