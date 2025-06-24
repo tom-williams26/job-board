e4bc9df6d91ff0b0a28f26f632bd7be1ce55e28a
Tom Williams <tom.williams0593@outlook.com>
2025-06-24 21:05:12 +0100

    Added a button to allow the user to apply for a job via the job card. Added JobPolicy for authorization when submitting a application.

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Job;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class JobApplicationController extends Controller
 {
+    use AuthorizesRequests;
     /**
      * Show the form for creating a new resource.
      */
     public function create(Job $job)
     {
+        $this->authorize('apply', $job);
         return view('job_application.create', ['job' => $job]);
     }
 
