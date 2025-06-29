4c3cc02b3ed5637ba02c149fbbeb75b6b1424041
Tom Williams <tom.williams0593@outlook.com>
2025-06-29 05:17:32 +0100

    Authenticated users can view the applications that they have submitted.  A new view is created  with reused components to view applied jobs.

---
+++ app/Http/Controllers/MyJobApplicationController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyJobApplicationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+        return view(
+            'my_job_application.index',
+            [
+                'applications' => auth()->user()->jobApplications()
+                    ->with('job', 'job.employer')
+                    ->latest()->get()
+            ]
+        );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
