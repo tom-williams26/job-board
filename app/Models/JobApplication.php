94cf26377b1dba13491502a7ffd2a75a3f091877
Tom Williams <tom.williams0593@outlook.com>
2025-07-01 23:40:50 +0100

    Created an additional migration so that a new column in the job_applications is added. The new column allows for a cv-path. This allows users to upload CVs to the database.

--- app/Models/JobApplication.php
+++ app/Models/JobApplication.php
@@ -10,7 +10,8 @@ class JobApplication extends Model
 {
     /** @use HasFactory<\Database\Factories\JobApplicationFactory> */
     use HasFactory;
-    protected $fillable = ['expected_salary', 'user_id', 'job_id'];
+
+    protected $fillable = ['expected_salary', 'user_id', 'job_id', 'cv_path'];
 
     public function job(): BelongsTo
     {
--- resources/views/job_application/create.blade.php
+++ resources/views/job_application/create.blade.php
@@ -13,6 +13,7 @@
         <form
             action="{{ route('job.application.store', $job) }}"
             method="POST"
+            enctype="multipart/form-data"
         >
             @csrf
             <div class="mb-4">
@@ -25,6 +26,15 @@
                     name="expected_salary"
                 />
             </div>
+
+            <div class="mb-4">
+                <label class="mb-2 block text-sm font-medium text-slate-900">Upload CV</label>
+                <x-text-input
+                    type="file"
+                    name="cv"
+                />
+            </div>
+
             <x-button class="w-full">Apply</x-button>
         </form>
     </x-card>
