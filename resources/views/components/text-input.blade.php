1e343268d4276472c1d25b416d3a92b29670a4bf
Tom Williams <tom.williams0593@outlook.com>
2025-07-02 00:32:59 +0100

    Input validation for when users apply to jobs.

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,34 +1,57 @@
 <div class="relative">
-    @if ($formRef)
-        <button
-            type="button"
-            class="absolute right-0 top-0 flex h-full cursor-pointer items-center pr-2"
-            @click="$refs['input-{{ $name }}'].value = ''; $refs['{{ $formRef }}'].submit();"
-        >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="h4 w-4 text-slate-500"
+
+    @if ('textarea' != $type)
+        @if ($formRef)
+            <button
+                type="button"
+                class="absolute right-0 top-0 flex h-full cursor-pointer items-center pr-2"
+                @click="$refs['input-{{ $name }}'].value = ''; $refs['{{ $formRef }}'].submit();"
             >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M6 18 18 6M6 6l12 12"
-                />
-            </svg>
-        </button>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="h4 w-4 text-slate-500"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M6 18 18 6M6 6l12 12"
+                    />
+                </svg>
+            </button>
+        @endif
+
+        <input
+            x-ref="input-{{ $name }}"
+            type="{{ $type }}"
+            placeholder="{{ $placeholder }}"
+            name="{{ $name }}"
+            value="{{ old($name, $value) }}"
+            id="{{ $name }}"
+            @class([
+                'w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2',
+                'pr-8' => $formRef,
+                'ring-slate-300' => !$errors->has($name),
+                'ring-red-300' => $errors->has($name),
+            ])
+        />
+    @else
+        <textarea
+            id="{{ $name }}"
+            name="{{ $name }}"
+            @class([
+                'w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2',
+                'pr-8' => $formRef,
+                'ring-slate-300' => !$errors->has($name),
+                'ring-red-300' => $errors->has($name),
+            ])
+        >{{ old($name, $value) }}</textarea>
     @endif
 
-    <input
-        x-ref="input-{{ $name }}"
-        type="{{ $type }}"
-        placeholder="{{ $placeholder }}"
-        name="{{ $name }}"
-        value="{{ $value }}"
-        id="{{ $name }}"
-        class="placeholder:text-slatee-400 w-full rounded-md border-0 px-2.5 py-1.5 pr-8 text-sm ring-1 ring-slate-300 focus:ring-2"
-    />
+    @error($name)
+        <div class="mt-1 text-xs text-red-500">{{ $message }}</div>
+    @enderror
 </div>
