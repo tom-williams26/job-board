1fc5407c592ce8b3cd993f625f913792e742204f
Tom Williams <tom.williams0593@outlook.com>
2025-07-03 21:29:37 +0100

    Added a route where authenticated users can check their jobs. Checks if the user is already an Employer via middleware and will prompt them to register as an employer if they're not registered.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -28,6 +28,11 @@
                         </a>
                     </li>
                     <li>
+                        <a href="{{ route('my-jobs.index') }}">
+                            My Jobs
+                        </a>
+                    </li>
+                    <li>
                         <form
                             action="{{ route('auth.destroy') }}"
                             method="POST"
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\MyJobApplicationController;
+use App\Http\Controllers\MyJobController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('', fn() => to_route('jobs.index'));
@@ -30,4 +31,7 @@ Route::middleware('auth')->group(function () {
 
     Route::resource('employer', EmployerController::class)
         ->only(['create', 'store']);
+
+    Route::middleware('employer')
+        ->resource('my-jobs', MyJobController::class);
 });
